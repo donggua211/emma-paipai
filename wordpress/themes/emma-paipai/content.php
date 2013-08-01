f0b456a5f35fa96bbbab340b6237b412e5be80f3
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-01 08:32:35 +0000

    -update

--- wordpress/themes/emma-paipai/content.php
+++ wordpress/themes/emma-paipai/content.php
@@ -8,21 +8,14 @@
  */
 ?>
 
-	<div id="post-<?php the_ID(); ?>" <?php post_class('entry_wrap'); ?>>
+	<div id="post-<?php the_ID(); ?>" <?php post_class('entry-wrap'); ?>>
 		<div class="entry-header">
 			<?php the_post_thumbnail(); ?>
-			<?php if ( is_single() ) : ?>
-				<?php the_title(); ?>
+			<?php if ( is_weibo() ) : ?>
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_author(); ?></a>
 			<?php else : ?>
-				<?php
-				$weibo_id 	= get_post_meta( get_the_ID(), '_weibo_id', true );
-				if ( empty( $weibo_id ) ) :
-				?>
-					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				<?php else : ?>
-					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_author(); ?></a>
-				<?php endif; // empty( $weibo_id ) ?>
-			<?php endif; // is_single() ?>
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+			<?php endif; // is_weibo() ?>
 		</div><!-- .entry-header -->
 
 		
@@ -40,7 +33,7 @@
 		$image_image = $image_image ? unserialize( $image_image ) : array();
 		
 		if( !empty( $image_weibo ) || !empty( $image_image ) )
-			echo '<div id="gallery" class="gallery entry-images">';
+			echo '<div class="entry-images">';
 		
 		foreach($image_weibo as $val)
 			echo '<a href="' . $val . '/2000" rel="prettyPhoto'.get_the_ID().'[pp_gal]" > <img src="' . $val . '/160" /></a>';
@@ -62,21 +55,5 @@
 			
 			<?php edit_post_link( '编辑', '<span>', '</span>' ); ?>
 			
-			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries. ?>
-				<div class="author-info">
-					<div class="author-avatar">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'emmapaipai_author_bio_avatar_size', 68 ) ); ?>
-					</div><!-- .author-avatar -->
-					<div class="author-description">
-						<h2><?php printf( __( 'About %s', 'emmapaipai' ), get_the_author() ); ?></h2>
-						<p><?php the_author_meta( 'description' ); ?></p>
-						<div class="author-link">
-							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'emmapaipai' ), get_the_author() ); ?>
-							</a>
-						</div><!-- .author-link	-->
-					</div><!-- .author-description -->
-				</div><!-- .author-info -->
-			<?php endif; ?>
 		</div><!-- .entry-meta -->
 	</div><!-- #post -->
