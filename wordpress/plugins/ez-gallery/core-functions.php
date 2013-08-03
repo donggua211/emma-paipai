79809fb124c249a5cee557099e4fe2b3a3991c80
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-03 09:29:04 +0000

    -update

--- wordpress/plugins/ez-gallery/core-functions.php
+++ wordpress/plugins/ez-gallery/core-functions.php
@@ -1,1 +1,6 @@
 <?php
+
+function get_gallery_page_id() {
+	$page = get_option('ez_gallery_page_id');
+	return ( $page ) ? $page : -1;
+}
---
+++ wordpress/plugins/ez-gallery/hook-functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Handle redirects before content is output - hooked into template_redirect so is_page works.
+ *
+ * @access public
+ * @return void
+ */
+function odsea_template_redirect() {
+	global $odsea, $wp_query;
+
+	// When default permalinks are enabled, redirect shop page to post type archive url
+	if ( ! empty( $_GET['page_id'] ) && $_GET['page_id'] == get_gallery_page_id() ) {
+		wp_safe_redirect( get_post_type_archive_link('ez_gallery') );
+		exit;
+	}
+}
+
+add_action( 'template_redirect', 'odsea_template_redirect' );
+
+
+
+function ez_gellary_pre_post( $q ) {
+	if ( is_post_type_archive( 'ez_gallery' ) )
+		$q->set( 'posts_per_page', -1 );
+}
+
+add_filter( 'pre_get_posts', 'ez_gellary_pre_post' );
