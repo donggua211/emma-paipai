53db7308a2a090114b5343004f93034da0c77189
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-11 04:00:13 +0000

    -update

--- trunk/wordpress/plugins/ez-gallery/hook-functions.php
+++ trunk/wordpress/plugins/ez-gallery/hook-functions.php
@@ -10,7 +10,7 @@ function odsea_template_redirect() {
 	global $odsea, $wp_query;
 
 	// When default permalinks are enabled, redirect shop page to post type archive url
-	if ( ! empty( $_GET['page_id'] ) && $_GET['page_id'] == get_gallery_page_id() ) {
+	if ( is_page( get_gallery_page_id() ) ) {
 		wp_safe_redirect( get_post_type_archive_link('ez_gallery') );
 		exit;
 	}
