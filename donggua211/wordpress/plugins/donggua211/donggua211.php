879fe71f097140043d1a1337437ad0e2dfd2cb04
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-11 05:47:42 +0000

    -Add favicon and Google Tracking codes

--- donggua211/wordpress/plugins/donggua211/donggua211.php
+++ donggua211/wordpress/plugins/donggua211/donggua211.php
@@ -9,3 +9,31 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+function donggua211_favicon(){
+	$output = '<link rel="shortcut icon" type="image/x-icon" href="' . plugins_url( '/favicon.ico', __FILE__ ) . '">';
+	
+	if ( $output != '' )
+		echo stripslashes( $output ) . "\n";
+}
+add_action( 'wp_head','donggua211_favicon' );
+
+
+function donggua211_analytics(){
+	$output = "
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42736021-2', 'emma-paipai.com');
+  ga('send', 'pageview');
+
+</script>";
+	
+	if ( $output != '' )
+		echo stripslashes( $output ) . "\n";
+}
+add_action( 'wp_footer','donggua211_analytics' );
