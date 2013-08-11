affaa83611865e2e0f0ad14fb1134b3480354b12
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-11 05:13:08 +0000

    -update

---
+++ trunk/donggua211/wordpress/plugins/donggua211/donggua211.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: donggua211.emma-paipai.com
+ * Plugin URI: http://www.emma-paipai.com/plugin/donggua211/
+ * Description: This is a plugin for donggua211.emma-paipai.com.
+ * Author: Yuan Zhao
+ * Version: 1.0
+ * Author URI: http://www.emma-paipai.com/donggua211/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
