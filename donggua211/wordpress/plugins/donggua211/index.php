e4606eeba6d54afab900143fb4ef3ee64917187e
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-08-21 05:05:51 +0000

    -update

---
+++ donggua211/wordpress/plugins/donggua211/index.php
@@ -0,0 +1,6 @@
+<div style=\"display:none;\">
+<a href=\"http://www.ogamebeta.com/\" title=\"Ogame\" target=\"_blank\">Ogame</a>
+</div>
+
+
+<a href=\"http://www.ogamebeta.com\" title=\"ogame\" target=\"_blank\">ogame</a>
