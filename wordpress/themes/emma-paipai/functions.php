d9cc482ecfb219cb8a58e3b256d0efddf28e593b
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2013-09-29 00:51:55 +0000

    -make page navigation links for index page

--- wordpress/themes/emma-paipai/functions.php
+++ wordpress/themes/emma-paipai/functions.php
@@ -262,7 +262,6 @@ function emmapaipai_content_nav( $html_id ) {
 
 	if ( $wp_query->max_num_pages > 1 ) : ?>
 		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
-			<h3 class="assistive-text"><?php _e( 'Post navigation', 'emmapaipai' ); ?></h3>
 			<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'emmapaipai' ) ); ?></div>
 			<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'emmapaipai' ) ); ?></div>
 		</nav><!-- #<?php echo $html_id; ?> .navigation -->
