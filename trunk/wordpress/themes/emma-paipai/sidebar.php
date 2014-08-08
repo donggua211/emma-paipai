8595ec4a0e013e699352d59ca2158acfda9491ea
zhaoyuan211 <zhaoyuan211@ddf5612c-120b-72ff-c150-3b15feae8f1c>
2014-08-08 22:29:10 +0000

    -Update

--- trunk/wordpress/themes/emma-paipai/sidebar.php
+++ trunk/wordpress/themes/emma-paipai/sidebar.php
@@ -10,6 +10,11 @@
  */
 ?>
 
+
+<div class="widget-area">
+	<a href="http://count.chanet.com.cn/click.cgi?a=515855&d=373368&u=&e=" target="_blank"><IMG SRC="http://file.chanet.com.cn/image.cgi?a=515855&d=373368&u=&e=" width="180" height="150"  border="0"></a>
+</div>
+
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<div id="secondary" class="widget-area">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
